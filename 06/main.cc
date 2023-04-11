5896ca32a0adfbe292f3613e459d5ecef47be743
zfazek <ongroa@gmail.com>
2023-04-11 07:18:45 +0200

    add 06 c++ solution

---
+++ 06/main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool is_palindrome(const string& str) {
+    for (size_t i = 0; i < str.size() / 2; ++i) {
+        const size_t j = str.size() - i - 1;
+        if (str[i] != str[j]) return false;
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    str = "abba";cout << str << " " << (is_palindrome(str) ? "true" : "false") << "\n";
+    str = "abca";cout << str << " " << (is_palindrome(str) ? "true" : "false") << "\n";
+    str = "a";cout << str << " " << (is_palindrome(str) ? "true" : "false") << "\n";
+    str = "";cout << str << " " << (is_palindrome(str) ? "true" : "false") << "\n";
+
+}
