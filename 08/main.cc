2ad871a7008695ff0be36b6bad941cb9cb87d49f
zfazek <ongroa@gmail.com>
2023-04-11 07:41:28 +0200

    add 08 c++ solution

---
+++ 08/main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+#include "../util.h"
+
+using namespace std;
+
+vector<char> compress(const vector<char>& list) {
+    vector<char> res;
+    for (size_t i = 0; i < list.size(); ++i) {
+        const char e = list.at(i);
+        if (res.empty()) {
+            res.push_back(e);
+            continue;
+        }
+        const char last = res.at(res.size() - 1);
+        if (e != last) {
+            res.push_back(e);
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<char> list;
+    list = {'a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'};
+    cout << print(list) << " " << print(compress(list)) << "\n";
+    list = {'a', 'a', 'c', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'a'};
+    cout << print(list) << " " << print(compress(list)) << "\n";
+    list = {'a', 'a', 'a'};
+    cout << print(list) << " " << print(compress(list)) << "\n";
+    list = {'a'};
+    cout << print(list) << " " << print(compress(list)) << "\n";
+    list = {};
+    cout << print(list) << " " << print(compress(list)) << "\n";
+}
